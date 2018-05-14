36e159f380fbc3749822ccf88febc64fce098cac
Abdulla Anas <abdullaanasanu@gmail.com>
2018-05-14 22:18:55 +0530

    Adding class completed & created database schema

--- base.js
+++ base.js
@@ -9,7 +9,24 @@ jQuery(document).ready(function(){
         division_name: jQuery('#edit-division').val(),
         token: mytoken
       },
-      success:function(data
+      success:function(data){
+        jQuery('<div id="messages"><div class="section clearfix"><div class="messages status"><h2 class="element-invisible">Status message</h2>'+data+'</div></div></div>').insertAfter('#header');
+      },
+      error:function(){
+        jQuery('<div id="messages"><div class="section clearfix"><div class="messages error"><h2 class="element-invisible">Error message</h2>Every Field is need to fill up!</div></div></div>').insertAfter('#header');
+      }
+    });
+  });
+  jQuery('#studentData').click(function(){
+    jQuery.ajax({
+      method: 'GET',
+      url: 'student_adding',
+      data:{
+        class_name: jQuery('#edit-name').val(),
+        division_name: jQuery('#edit-division').val(),
+        token: mytoken
+      },
+      success:function(data){
         jQuery('<div id="messages"><div class="section clearfix"><div class="messages status"><h2 class="element-invisible">Status message</h2>'+data+'</div></div></div>').insertAfter('#header');
       },
       error:function(){
