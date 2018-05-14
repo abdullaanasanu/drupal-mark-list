ff470ae123132f48c6e51797de3cc1b679b7639f
Abdulla Anas <abdullaanasanu@gmail.com>
2018-05-14 18:01:45 +0530

    base.js added with Ajax submit function and MarkList.module updated

---
+++ base.js
@@ -0,0 +1,20 @@
+
+jQuery(document).ready(function(){
+  jQuery('#classData').click(function(){
+    jQuery.ajax({
+      method: 'GET',
+      url: 'class_adding',
+      data:{
+        class_name: jQuery('#edit-name').val(),
+        division_name: jQuery('#edit-division').val(),
+        token: mytoken
+      },
+      success:function(data
+        jQuery('<div id="messages"><div class="section clearfix"><div class="messages status"><h2 class="element-invisible">Status message</h2>'+data+'</div></div></div>').insertAfter('#header');
+      },
+      error:function(){
+        jQuery('<div id="messages"><div class="section clearfix"><div class="messages error"><h2 class="element-invisible">Error message</h2>Every Field is need to fill up!</div></div></div>').insertAfter('#header');
+      }
+    });
+  });
+});
